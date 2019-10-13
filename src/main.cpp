5f0ad85c1f3f374d3d8c4c485f4a590a9b079c40
Tomasz Gajger <gajger.tomasz@gmail.com>
2019-10-13 15:55:41 +0200

    Add input validation

--- src/main.cpp
+++ src/main.cpp
@@ -2,10 +2,51 @@
 #include "listener.hpp"
 #include <boost/algorithm/string.hpp>
 #include <opencv2/imgcodecs.hpp>
+#include <tuple>
+#include <thread>
+
+using namespace std::chrono_literals;
 
 void initialize_heat_compute();
 void basic_heat_compute();
 std::vector<std::vector<float>> cuda_heat_compute(int blockDimX, int blockDimY, int meshSize, int steps);
+
+std::tuple<int, int, int, int> parseParams(const utility::string_t& queryString) {
+	std::vector<utility::string_t> params;
+	boost::split(params, queryString, boost::is_any_of("&"));
+	for (const auto& param : params) {
+		ucout << U("Param: ") << param << U("\n");
+	}
+
+	if (params.size() != 4) {
+		throw std::invalid_argument{ "Invalid number of request paramers" };
+	}
+
+	return { std::stoi(params[0]), std::stoi(params[1]), std::stoi(params[2]), std::stoi(params[3]) };
+}
+
+auto encodeImage(const std::vector<std::vector<float>>& grid) {
+	cv::Mat image(static_cast<int>(grid.front().size()), static_cast<int>(grid.size()), CV_8UC3);
+	for (int y = 0; y < image.rows; ++y) {
+		for (int x = 0; x < image.cols; ++x) {
+			auto& bgra = image.at<cv::Vec3b>(x, y);
+			//TODO: proper color representation
+			//bgra[0] = UCHAR_MAX; // Blue
+			//bgra[1] = cv::saturate_cast<uchar>((float(image.cols - j)) / ((float)image.cols) * UCHAR_MAX); // Green
+			//bgra[2] = cv::saturate_cast<uchar>((float(image.rows - i)) / ((float)image.rows) * UCHAR_MAX); // Red
+			bgra[0] = 0; // Blue
+			bgra[1] = 0; // Green
+			bgra[2] = static_cast<int>(grid[y][x] * 4) % UCHAR_MAX; // Red
+		}
+	}
+
+	std::vector<unsigned char> encodedImage;
+	if (!cv::imencode(".png", image, encodedImage, { cv::IMWRITE_PNG_COMPRESSION, 9 })) {
+		throw std::runtime_error{ "Failed to encode the image" };
+	}
+
+	return encodedImage;
+}
 
 int main(int argc, char* argv[])
 {
@@ -18,55 +59,41 @@ int main(int argc, char* argv[])
 	listener.addListener(L"test", [](auto req) -> web::http::http_response {
 		const auto queryString{ req.request_uri().query() };
 		ucout << U("Request received: ") << queryString << std::endl;
-
-		//TODO: Validate query string properly, number of arguments, args type
-
-		std::vector<utility::string_t> params;
-		boost::split(params, queryString, boost::is_any_of("&"));
-		for (const auto& param : params) {
-			ucout << U("Param: ") << param << U("\n");
+		ucout << U("Remote address: ") << req.remote_address()<< std::endl;
+		ucout << U("Headers: ") << std::endl;
+		for (const auto&[header, content] : req.headers()) {
+			ucout << U("   ") << header << U(" = ") << content << std::endl;
 		}
 
-		if (params.size() != 4) {
-			std::cerr << "Invalid number of request paramers\n";
+		try {
+			const auto [blockX, blockY, meshSize, steps]{ parseParams(queryString) };
+			std::cout << "Computing result...\n";
+			const auto result{ cuda_heat_compute(blockX, blockY, meshSize, steps) };
+			if (result.empty()) {
+				std::cerr << "Failed to compute result\n";
+				return web::http::status_codes::InternalError;
+			}
+
+			web::http::http_response response{ web::http::status_codes::OK };
+			response.set_body(encodeImage(result));
+			response.headers().set_content_type(U("image/png"));
+			return response;
+		}
+		catch (const std::invalid_argument& err) {
+			std::cerr << err.what() << '\n';
 			return web::http::status_codes::BadRequest;
 		}
-
-		std::cout << "Computing result...\n";
-		const auto result{ cuda_heat_compute(std::stoi(params[0]), std::stoi(params[1]), std::stoi(params[2]), std::stoi(params[3])) };
-		if (result.empty()) {
-			std::cerr << "Failed to compute result\n";
+		catch (const std::runtime_error& err) {
+			std::cerr << err.what() << '\n';
 			return web::http::status_codes::InternalError;
 		}
-
-		cv::Mat image( static_cast<int>(result.front().size()), static_cast<int>(result.size()), CV_8UC3 );
-		for (int y = 0; y < image.rows; ++y) {
-			for (int x = 0; x < image.cols; ++x) {
-				auto& bgra = image.at<cv::Vec3b>(x, y);
-				//bgra[0] = UCHAR_MAX; // Blue
-				//bgra[1] = cv::saturate_cast<uchar>((float(image.cols - j)) / ((float)image.cols) * UCHAR_MAX); // Green
-				//bgra[2] = cv::saturate_cast<uchar>((float(image.rows - i)) / ((float)image.rows) * UCHAR_MAX); // Red
-				bgra[0] = 0; // Blue
-				bgra[1] = 0; // Green
-				bgra[2] = static_cast<int>(result[y][x] * 4) % UCHAR_MAX; // Red
-			}
-		}
-
-		std::vector<unsigned char> encodedImage;
-		if (!cv::imencode(".png", image, encodedImage, { cv::IMWRITE_PNG_COMPRESSION, 9 })) {
-			std::cerr << "Failed to encode the image\n";
-			return web::http::status_codes::InternalError;
-		}
-
-		web::http::http_response response{ web::http::status_codes::OK };
-		response.set_body(encodedImage);
-		response.headers().set_content_type(U("image/png"));
-		return response;
 	});
 
 	listener.start();
 
-	while (running);
+	while (running) {
+		std::this_thread::sleep_for(1s);
+	}
 
 	listener.stop();
 
